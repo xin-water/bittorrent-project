547e84635f927b3676554fcd539f440ece205a0b
xinwater <1872407021@qq.com>
2021-01-18 16:56:27 +0800

    remove handshake module

--- bittorrent-protocol/src/handshake/mod.rs
+++
@@ -1,9 +0,0 @@
-mod bittorrent;
-//mod channel;
-mod handshaker;
-
-pub use self::bittorrent::BTHandshaker;
-pub use self::bittorrent::BTPeer;
-pub use self::handshaker::Handshaker;
-
-pub use crate::util::bt::{InfoHash, PeerId};
