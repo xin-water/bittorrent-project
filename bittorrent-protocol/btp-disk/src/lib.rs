9c3309af110c6e1a6539d9437475bb4eddf14414
xinwater <1872407021@qq.com>
2022-09-30 14:03:09 +0800

    btp_disk v0.1.0 patch

--- bittorrent-protocol/btp-disk/src/lib.rs
+++ bittorrent-protocol/btp-disk/src/lib.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
-}
+
+#[macro_use]
+extern crate log;
+
+#[macro_use]
+extern crate error_chain;
+
+mod memory;
+pub use self::memory::block::{Block, BlockMetadata, BlockMut};
+
+pub mod fs;
+pub use self::fs::cache::file_handle::FileHandleCache;
+pub use self::fs::native::{NativeFile, NativeFileSystem};
+pub use self::fs::FileSystem;
+
+pub mod message;
+pub use self::message::{IDiskMessage, ODiskMessage};
+
+mod tasks;
+
+pub mod manager;
+pub use self::manager::{DiskManager, DiskManagerSink, DiskManagerStream};
+
+pub mod builder;
+pub use self::builder::DiskManagerBuilder;
+
+/// Both `Block` and `Torrent` error types.
+pub mod error;
+
+pub use btp_util::bt::InfoHash;
