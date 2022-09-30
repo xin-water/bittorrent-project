764bb8f93f6e5fa13f6a5e7a7f57c4eccde0fddd
xinwater <1872407021@qq.com>
2022-09-30 14:16:27 +0800

    btp-utracker v0.1.0 patch

--- bittorrent-protocol/btp-utracker/src/lib.rs
+++ bittorrent-protocol/btp-utracker/src/lib.rs
@@ -1,14 +1,33 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+//! Library for parsing and writing UDP tracker messages.
+//!
+//! Includes a default implementation of a bittorrent UDP tracker client
+//! and a customizable trait based implementation of a bittorrent UDP tracker
+//! server.
+// Action ids used in both requests and responses.
+const CONNECT_ACTION_ID: u32 = 0;
+const ANNOUNCE_IPV4_ACTION_ID: u32 = 1;
+const SCRAPE_ACTION_ID: u32 = 2;
+const ANNOUNCE_IPV6_ACTION_ID: u32 = 4;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[macro_use]
+extern crate nom;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod request;
+pub mod response;
+
+pub mod announce;
+pub mod contact;
+pub mod error;
+pub mod option;
+pub mod scrape;
+
+mod client;
+mod server;
+
+pub use client::error::{ClientError, ClientResult};
+pub use client::{ClientMetadata, ClientRequest, ClientResponse, ClientToken, TrackerClient,Handshaker};
+
+pub use server::handler::{ServerHandler, ServerResult};
+pub use server::TrackerServer;
+
+pub use btp_util::bt::{InfoHash, PeerId};
