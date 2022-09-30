dec4eccc09915f314368665afd37b39f6a0ba231
xinwater <1872407021@qq.com>
2022-09-30 14:02:16 +0800

    btp_util v0.1.0 patch

--- bittorrent-protocol/btp-util/src/lib.rs
+++ bittorrent-protocol/btp-util/src/lib.rs
@@ -1,14 +1,74 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//! Utilities used by the Bittorrent Infrastructure Project.
+
+/// Bittorrent specific types.
+pub mod bt;
+
+/// Arrays of buffers as a contiguous buffer.
+pub mod contiguous;
+
+/// Converting between data.
+pub mod convert;
+
+/// Networking primitives and helpers.
+pub mod net;
+
+/// Generic sender utilities.
+pub mod send;
+
+/// Hash primitives and helpers.
+pub mod sha;
+
+/// Testing fixtures for dependant crates.
+/// TODO: Some non test functions in other crates use this, mark that as cfg test
+/// when we migrate away from these functions in non test functions.
+pub mod test;
+
+/// Generating transaction ids.
+pub mod trans;
+
+/// Common error types.
+pub mod error;
+
+//----------------------------------------------------------------------------//
+use std::mem;
+
+/// Applies a Fisher-Yates shuffle on the given list.
+pub fn fisher_shuffle<T: Default>(list: &mut [T]) {
+    for i in 0..list.len() {
+        let swap_index = (rand::random::<usize>() % (list.len() - i)) + i;
+
+        // Can't push the src_val directly into the swap_index in case i and swap_index
+        // are the same value (we will end up setting our index to the default value).
+        let src_val = mem::replace(&mut list[i], T::default());
+        let dst_val = mem::replace(&mut list[swap_index], T::default());
+
+        list[i] = dst_val;
+        list[swap_index] = src_val;
+    }
+}
+
+/// Applies a Fisher-Yates shuffle on the given list.
+pub fn fisher_shuffle_copy<T: Copy>(list: &mut [T]) {
+    for i in 0..list.len() {
+        let swap_index = (rand::random::<usize>() % (list.len() - i)) + i;
+
+        let temp = list[i];
+        list[i] = list[swap_index];
+        list[swap_index] = temp;
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn positive_fisher_shuffle() {
+        let mut test_slice = [1, 2, 3, 4];
+
+        super::fisher_shuffle(&mut test_slice);
+
+        assert!(test_slice.contains(&1));
+        assert!(test_slice.contains(&2));
+        assert!(test_slice.contains(&3));
+        assert!(test_slice.contains(&4));
     }
 }
