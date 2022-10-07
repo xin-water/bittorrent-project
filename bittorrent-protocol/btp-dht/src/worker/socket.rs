89fe8aab1f9fe8a9a2ece69a9b3d843b9cd61a3c
xinwater <1872407021@qq.com>
2022-10-07 21:16:22 +0800

    直接拓展udp

--- bittorrent-protocol/btp-dht/src/worker/socket.rs
+++ bittorrent-protocol/btp-dht/src/worker/socket.rs
@@ -103,6 +103,63 @@ impl fmt::Display for IpVersion {
     }
 }
 
+// 直接拓展udp,不使用面向接口对象了。
+pub(crate) struct DhtSocket(UdpSocket,SocketAddr);
+impl DhtSocket{
+
+    pub fn new(inner: UdpSocket) -> io::Result<Self> {
+        let local_addr = inner.local_addr()?;
+        Ok(Self(inner, local_addr))
+    }
+
+    pub(crate) async fn send(&self, bytes: &[u8], addr: SocketAddr) -> Result<(),&str> {
+
+        let mut bytes_sent = 0;
+
+        while bytes_sent != bytes.len() {
+            if let Ok(num_sent) = self.0.send_to(&bytes[bytes_sent..], addr).await {
+                bytes_sent += num_sent;
+            } else {
+                // TODO: Maybe shut down in this case, will fail on every write...
+                warn!(
+                "bittorrent-protocol_dht: Socket Outgoing messenger failed to write {} bytes to {}; {} bytes written \
+                   before error...",
+                bytes.len(),
+                addr,
+                bytes_sent
+            );
+                return Err("DhtSocket data send fail");
+            }
+        }
+        Ok(())
+    }
+
+
+    pub(crate) async fn recv(&self) -> Result<(Vec<u8>, SocketAddr),&str> {
+        let mut buffer = vec![0u8; 1500];
+        if let Ok((size, addr)) = self.0.recv_from(&mut buffer).await{
+            buffer.truncate(size);
+            return Ok((buffer, addr));
+        }
+        Err("DhtSocket data recv error")
+    }
+
+    pub fn local_addr(&self) -> SocketAddr {
+        self.1
+    }
+
+    pub fn ip_version(&self) -> IpVersion {
+        match self.1 {
+            SocketAddr::V4(_) => IpVersion::V4,
+            SocketAddr::V6(_) => IpVersion::V6,
+        }
+    }
+
+}
+
+
+
+
 
 
 
